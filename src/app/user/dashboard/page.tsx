bd4ab062ba288551a5c4b0c8f0eb5a6f0a82d8f5
Chonang VIP <chonangrodung@gmail.com>
2025-05-12 16:07:40 +0100

    removed unused variables

--- src/app/user/dashboard/page.tsx
+++ src/app/user/dashboard/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useState, useEffect } from "react";
-import { createClient } from "@supabase/supabase-js";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useRouter } from "next/navigation";
